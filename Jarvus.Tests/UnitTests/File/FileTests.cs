65545a0cd87f8b20fafdb016418f5c8810254d6d
Mike Gunn <mgunn@jarv.us>
2017-12-01 16:40:44 -0500

    Unit test to confirm that correct AbsolutePath for differnt constructors is returned

--- Jarvus.Tests/UnitTests/File/FileTests.cs
+++ Jarvus.Tests/UnitTests/File/FileTests.cs
@@ -8,9 +8,13 @@ namespace Jarvus.Tests.UnitTests
         [Fact]
         public void Test1()
         {
-            var file = new Jarvus.File.File() {};
+            var file1 = new Jarvus.File.File("/home/mgunn/tmp/avatar.jpg");
+            var file2 = new Jarvus.File.File(){
+                AbsoluteBase = "/home/mgunn"
+                ,RelativePath = "tmp/avatar.jpg"
+            };
 
-            Assert.Equal(file, null);
+            Assert.Equal(file1.AbsolutePath, file2.AbsolutePath);
         }
     }
 }
