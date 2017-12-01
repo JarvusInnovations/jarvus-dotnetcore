970b256c90c3a6541753e406c928c323d3a6f8c8
Jim Smiley <skiver.kelvin@gmail.com>
2017-12-01 15:38:58 -0500

    adds integration and unit test strucutres

---
+++ Jarvus.Tests/IntegrationTests/README.md
@@ -0,0 +1,1 @@
+Directory added for future use.
---
+++ Jarvus.Tests/UnitTests/File/FileTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Jarvus.Tests.UnitTests
+{
+    public class FileTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
